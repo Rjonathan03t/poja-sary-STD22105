7b13f1a43785a71b1a7c353f8f124df94db6d227
Jonathan Rajao <hei.tovomihaja@gmail.com>
2024-02-21 23:15:22 +0300

    feat:add upload controller

---
+++ src/main/java/school/hei/sary/endpoint/rest/controller/health/BlackAndWhiteController.java
@@ -0,0 +1,27 @@
+package school.hei.sary.endpoint.rest.controller.health;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.hei.sary.file.BucketComponent;
+import school.hei.sary.file.FileHash;
+
+import java.io.File;
+
+@RestController
+public class BlackAndWhiteController {
+    BucketComponent bucketComponent;
+    @PostMapping ("/upload/{bucketKey}")
+    public ResponseEntity<String> uploadFileToS3(
+            @RequestBody File file,
+            @PathVariable String bucketKey) {
+        try {
+            FileHash fileHash = bucketComponent.upload(file, bucketKey);
+            return ResponseEntity.ok("File uploaded successfully to S3. Hash: " + fileHash);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file to S3.");
+        }
+    }
+}
